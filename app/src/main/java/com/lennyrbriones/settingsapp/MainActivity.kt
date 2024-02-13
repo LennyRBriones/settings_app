390dff48ff3b8f735c7442259d5cfbb360a35a07
LennyRBriones <lennyrbriones@outlook.com>
2024-02-13 01:26:31 -0600

    Linear Layouts, material divider, viewbinding, switchmaterial, rangeslider

--- app/src/main/java/com/lennyrbriones/settingsapp/MainActivity.kt
+++ app/src/main/java/com/lennyrbriones/settingsapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.lennyrbriones.settingsapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lennyrbriones.settingsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
